770375de4797db8203b8d6c85809532fda85dcc2
MikolajKorbanek <korbanek.mikolaj@gmail.com>
2017-12-04 20:29:51 +0100

    FINISHED

---
+++ 1_UnitConverter_CSharp/1_UnitConverter_CSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _1_UnitConverter_CSharp
+{
+    class UnitConverter
+    {
+        String option ="";
+       
+        public UnitConverter() {
+            do {
+                String value = "";
+                Console.WriteLine("1. M/KM");
+                Console.WriteLine("2. CM/M");
+                Console.WriteLine("3. KG/G");
+                Console.WriteLine("4. Exit");
+                Console.Write("Wybierz opcje: ");
+                option = Console.ReadLine();
+
+                switch (int.Parse(option)) {
+                    case 1:
+                        Console.Write("Podaj wartosc w metrach: ");
+                        value = Console.ReadLine();
+                        Console.WriteLine(float.Parse(option) / 1000);
+                        Console.WriteLine("Wcisnij przycisk");
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.Write("Podaj wartosc w centymetrach: ");
+                        value = Console.ReadLine();
+                        Console.WriteLine(float.Parse(option) / 100);
+                        Console.WriteLine("Wcisnij przycisk");
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.Write("Podaj wartosc w kilogramach: ");
+                        value = Console.ReadLine();
+                        Console.WriteLine(float.Parse(option) * 1000);
+                        Console.WriteLine("Wcisnij przycisk");
+                        Console.WriteLine();
+                        Console.ReadKey();
+                        break;
+                }
+
+            }while (int.Parse(option) != 4);
+        }
+
+
+        static void Main(string[] args)
+        {
+            new UnitConverter();
+        }
+    }
+}
