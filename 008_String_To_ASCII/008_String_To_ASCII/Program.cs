9a0e0c87690813c93d407ebd76611ea85648001a
MikolajKorbanek <korbanek.mikolaj@gmail.com>
2018-01-09 18:39:15 +0100

    FINISHED

---
+++ 008_String_To_ASCII/008_String_To_ASCII/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_String_To_ASCII {
+    class Program {
+
+        static public string Convert(string text) {
+            string result = "";
+
+            foreach(char c in text) {
+                result += (int)c;
+                result += " ";
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args) {
+            Console.Write("Podaj text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(Program.Convert(text));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 008_String_To_ASCII/008_String_To_ASCII/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("008_String_To_ASCII")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("008_String_To_ASCII")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("db0e288f-a3cc-434b-a68f-f98139c30fd3")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
