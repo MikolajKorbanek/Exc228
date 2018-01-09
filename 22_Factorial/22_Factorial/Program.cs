a6b9d094e302e0a02743c933dc5fab27d0276288
MikolajKorbanek <korbanek.mikolaj@gmail.com>
2018-01-09 18:27:59 +0100

    FINISHED

---
+++ 22_Factorial/22_Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_Factorial {
+    class Program {
+
+        static int Factorial(int value) {
+            int result = 1;
+
+            for(int i = value; i > 0; i--) {
+                result *= i;
+            }
+
+
+            return result;
+        }
+
+
+        static void Main(string[] args) {
+            Console.Write("Podaj liczbe z jakiej chcesz silnie: ");
+            int value = Int32.Parse(Console.ReadLine());
+            Console.Write("{0}! to {1}", value, Program.Factorial(value));
+            Console.ReadKey();
+        }
+    }
+}
